caf2d4698c46acc94d4f63b406bf3e6be850bcf2
uzair_seikhai <uzairseikhai@gmail.com>
2020-09-13 08:48:25 +0500

    Master Layout implemented successfully

---
+++ footer.php
@@ -0,0 +1,223 @@
+	<footer id="footer" class="footer-04">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 ft1">
+					<div class="widget-footer widget-about">
+						<div class="logo-ft">
+							<a href="#" title="">
+								<!-- <img src="images/logo-ft.png" alt=""> -->
+								<img class="footer-logo" src="images/Logo transparent for Website.png" alt="">
+							</a>
+						</div>
+						<ul class="infomation-ft">
+							<li>
+								329 Queensberry Street, North Melbourne VIC 3051, Australia.
+							</li>
+							<li>123 456 7890</li>
+							<li>support@prodrive.com</li>
+						</ul>
+						<ul class="social-ft">
+							<a class="Font-Links" href="#">
+								<i class="fa fa-facebook font-Awesome"></i>
+							</a>
+							<a class="Font-Links " href="#">
+								<i class="fa fa-instagram font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-twitter font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-linkedin font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-google-plus font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-envelope-open font-Awesome"></i>
+							</a>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-2 ft2">
+					<div class="widget-footer widget-services">
+						<h3 class="title-ft">
+							Our services
+						</h3>
+						<ul>
+							<li>
+								<a href="#" title="">Airport transfer</a>
+							</li>
+							<li>
+								<a href="#" title="">Limousine service</a>
+							</li>
+							<li>
+								<a href="#" title="">Chauffeur service</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-2 ft2">
+					<div class="widget-footer widget-services">
+						<h3 class="title-ft">
+							About us
+						</h3>
+						<ul>
+							<li>
+								<a href="#" title="">Chauffeurs</a>
+							</li>
+							<li>
+								<a href="#" title="">About us</a>
+							</li>
+							<li>
+								<a href="#" title="">Blog</a>
+							</li>
+							<li>
+								<a href="#" title="">FAQs</a>
+							</li>
+							<li>
+								<a href="#" title="">Testimonials</a>
+							</li>
+							<li>
+								<a href="#" title="">Terms & Conditions</a>
+							</li>
+							<li>
+								<a href="#" title="">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-2 ft2">
+					<div class="widget-footer widget-services">
+						<h3 class="title-ft">
+							Our cars
+						</h3>
+						<ul>
+							<li>
+								<a href="#" title="">Chrysler 300</a>
+							</li>
+							<li>
+								<a href="#" title="">Bmw 750i</a>
+							</li>
+							<li>
+								<a href="#" title="">Rolls-Royce Phantom</a>
+							</li>
+							<li>
+								<a href="#" title="">Caddilac XTS</a>
+							</li>
+							<li>
+								<a href="#" title="">Mercedes S50</a>
+							</li>
+							<li>
+								<a href="#" title="">Chevrolet suburban </a>
+							</li>
+							<li>
+								<a href="#" title="">Hummer Strech Limousine </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3 ft1">
+					<div class="widget-footer newletter">
+						<h3 class="title-ft">
+							Newsletter
+						</h3>
+						<p class="content-newletter">Subscribe to our newsletter for news,
+							updates, exclusive discounts and offers.</p>
+						<form id="subscribe-form" action="#" method="post" accept-charset="utf-8"
+							data-mailchimp="true">
+							<!-- <div id="subscribe-content">
+								<div class="field-one-third">
+									<div class="field-name">
+										<input id="subscribe-email" type="text" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="btn-submit">
+									<button id="subscribe-button" type="button" class="base box-shadow"></button>
+								</div>
+							</div> -->
+							<div id="subscribe-msg">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+	<!-- Start Footer Bottom -->
+	<section class="footer-bottom bottom-04">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="title">
+						Our Top Cities
+					</div>
+					<ul class="city">
+						<li>
+							<a href="#" title="">New York</a>
+						</li>
+						<li>
+							<a href="#" title="">Istanbul</a>
+						</li>
+						<li>
+							<a href="#" title="">London</a>
+						</li>
+						<li>
+							<a href="#" title="">Berlin</a>
+						</li>
+						<li>
+							<a href="#" title="">Los Angeles</a>
+						</li>
+						<li>
+							<a href="#" title="">Paris</a>
+						</li>
+						<li>
+							<a href="#" title="">All cities</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Footer Bottom -->
+	<!-- Start Copyright -->
+	<section class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Copyright Prodrive © 2018. All Rights Reserved</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Copyright -->
+	<div class="scroll-top">
+	</div>
+	<!-- Javascript -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/parallax.js"></script>
+	<script type="text/javascript" src="js/waypoints.min.js"></script>
+	<script type="text/javascript" src="js/waves.min.js"></script>
+	<!--   -->
+	<script type="text/javascript" src="js/moment.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+	<script type="text/javascript" src="js/jquery.daterangepicker.js"></script>
+	<script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
+	<script type="text/javascript" src="js/template.js"></script>
+	<!-- Revolution Slider -->
+	<script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
+	<script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+	<script type="text/javascript" src="revolution/js/slider.js"></script>
+	<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!-- Basic Page Needs -->
+	<meta charset="UTF-8">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="author" content="CreativeLayers">
+	<title>Prodrive - Homepage 04</title>
+	<!-- Boostrap style -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-glyphicons.css">
+
+	<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
+	<!-- REVOLUTION LAYERS STYLES -->
+	<link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+	<link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive style -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<link rel="stylesheet" href="css/Dummy Css/Dummy.Css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<div id="loading-overlay">
+		<div class="loader"></div>
+	</div>
+	<!-- Start Header -->
+		<header id="header" class="header-04">
+		<div class="top-header">
+			<div class="container-fluid px-5">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-6 left-content">
+						<ul>
+							<li><i class="fa fa-phone mr-2 "></i><a class="Contact-us" href="tel:+18889211116">+1 (888) 921-1116</a></li>
+							<li><i class="fa fa-mobile mr-2"></i><a class="Contact-us" href="tel:+14373707407">+1 (437) 370-7407</a></li>
+							<li>
+								<i class="fa fa-envelope mr-2"></i>
+								<a class="Contact-us" href="https://platinumrides.ca">info@pearsonlimoservices.ca</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-6 right-content">
+						<div class="login">
+							<a class="Font-Links" href="#">
+								<i class="fa fa-facebook font-Awesome"></i>
+							</a>
+							<a class="Font-Links " href="#">
+								<i class="fa fa-instagram font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-twitter font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-linkedin font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-google-plus font-Awesome"></i>
+							</a>
+							<a class="Font-Links" href="#">
+								<i class="fa fa-envelope-open font-Awesome"></i>
+							</a>
+						</div>
+						<!-- <div class="top-menuleft no_bo">
+								<div class="x_language">
+									<img src="images/icon/web.png" alt="">
+									<select id="languages">
+									  <option value="saab">KR</option>
+									  <option value="vw">FR</option>
+									  <option value="audi" selected>TR</option>
+									</select>
+								</div>
+							</div> -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="bottom-header">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="logo col-1g-2 col-md-2 col-sm-6 col-6">
+						<img height="100%" width="100%" src="images/Logo transparent for Website.png" alt="">
+					</div>
+					<div class="col-1g-10 col-md-10 col-sm-6 col-6 mt-4">
+						<div class="navigation">
+							<div class="mobile-menu">
+								<span></span>
+							</div>
+							<div id="main-menu" class="main-menu">
+								<div class="top-text">
+									<span>
+										<!-- <a href="index.html" title=""><img src="images/logo.png" alt=""></a></span> -->
+										<!-- <a href="index.html" title=""><img style="height: 70px; width: 150px;" sty src="../Logo - Final (24 Jul 20)/Logo transparent for Website.png" alt=""></a></span> -->
+										<i class="pe-7s-close"></i>
+								</div>
+								<ul class="menu">
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="index.php">Home</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="index.html">Home 01</a></li>
+											<div class="border-down"></div>
+											<li><a href="index2.html">Home 02</a></li>
+											<div class="border-down"></div>
+											<li><a href="index3.html">Home 03</a></li>
+											<div class="border-down"></div>
+											<li><a href="index4.html">Home 04</a></li>
+											<div class="border-down"></div>
+											<li><a href="index5.html">Home 05</a></li>
+											<div class="border-down"></div>
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="our-fleet.php">Our Fleet</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="our-fleet-01.html">Our Fleet 01</a></li>
+											<li><a href="our-fleet-02.html">Our Fleet 02</a></li>
+											<li><a href="our-fleet-03.html">Our Fleet 03</a></li>
+											<li><a href="our-fleet-04.html">Our Fleet 04</a></li>
+											<li><a href="our-fleet-05.html">Our Fleet 05</a></li>
+											<li><a href="our-fleet-single1.html">Our Fleet Single 01</a></li>
+											<li><a href="our-fleet-single2.html">Our Fleet Single 02</a></li>
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">Service Rates</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="services.html">Services</a></li>
+											<li><a href="services-rates.html">Services Rates</a></li>
+											<li><a href="services-single.html">Services Single</a></li>
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">About</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="booking-car-class.html">Booking Car Class</a></li>
+											<li><a href="booking-card.html">Booking Card</a></li>
+											<li><a href="booking-check-out.html">Booking Checkout</a></li>
+											<li><a href="booking-login.html">Booking Login</a></li>
+											<li><a href="booking-options.html">Booking Options</a></li>
+											<li><a href="404.html">404 Page</a></li>
+											<li><a href="about1.html">About 01</a></li>
+											<li><a href="about2.html">About 02</a></li>
+											<li><a href="accordion.html">Accordion</a></li>
+											<li><a href="alert.html">Alert</a></li>
+											<li><a href="button.html">Button</a></li>
+											<li><a href="price-table.html">Price Table</a></li>
+											<li><a href="progress.html">Progress</a></li>
+											<li><a href="tables.html">Tables</a></li>
+											<li><a href="tabs.html">Tabs</a></li>
+											<li><a href="typography.html">Typography</a></li>
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">reservation</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="blog.html">Blog</a></li>
+											<li><a href="blog-grid.html">Blog Grid</a></li>
+											<li><a href="blog-sidebar.html">Blog Sidebar</a></li>
+											<li><a href="blog-fullwidth.html">Blog Fullwidth</a></li>
+											<li><a href="blog-fullwidth-sidebar.html">Blog Fullwidth Sidebar</a>
+											</li>
+											<li><a href="blog-single.html">Blog Single</a></li>
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">area we serve</a>
+										<ul class="menu-dropdown after-drop">
+
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">Services</a>
+										<ul class="menu-dropdown after-drop">
+
+										</ul>
+									</li>
+									<li class="has-dropdown Dropdown">
+										<a class="links" href="#">Contact</a>
+										<ul class="menu-dropdown after-drop">
+											<li><a href="contact1.html">Contact 01</a></li>
+											<li><a href="contact2.html">Contact 02</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+				</div>
+				<!-- <div class="box-right">
+						<div class="search">
+							<a href="#" class="view_search"><i class="pe-7s-search"></i></a>
+							<form class="form-search" method="get" action="#">
+								<input class="action-text" type="text" name="s" placeholder="Type & Hit Enter...">
+								<button type="submit" id="submit"><i class="pe-7s-search"></i></button>
+								<a href="#" class="close-fixed" title="Close"><img src="images/icon/close_s.png" alt=""></a>
+							</form>
+						</div>
+					</div> -->
+			</div>
+		</div>
+		</header>
+	<!-- End Header -->
