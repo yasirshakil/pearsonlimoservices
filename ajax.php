e922f74089bdc0fa5c6d2afda205f3501f13fe9c
shoaib <shoaib.raza.dev@gmail.com>
2020-09-25 14:25:03 -0700

    Ajax page pushed

---
+++ ajax.php
@@ -0,0 +1,142 @@
+<?php
+include ('./header.php');
+?>
+    <div class="container-fluid ">
+        <div class="container mt-5 mb-5 bgColor">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <h1 class="mb-2 AjaxLimo">Ajax Limousine Services</h1>
+                    <p class="mb-2 AreaDetails">Ajax, the town named after a royal navy cruiser, deserves a royal treatment. Therefore, we at
+                        Platinum Rides take pride in providing its finest Ajax limo service in the eastern part of GTA
+                        throughout the town of Ajax. Our Ajax airport shuttle service is available from Ajax to YYZ Pearson
+                        International Airport and vice versa throughout the year. Ajax Limo Rentals and Airport
+                        Transportation to/from Toronto Airport, Buffalo Airport, Niagara Airport and Hamilton Airport Our
+                        extraordinary and luxurious fleet of limousines are ready at your disposal in Ajax and its
+                        surrounding areas, to get a quote or make your reservation call us at
+                        <span style="color: #d9c77e;"><a style="color: #d9c77e;" href="tel:+14373707407">+1 (437)
+                                370-7407</a>
+                        </span> or visit:
+                        <span style="color: #d9c77e;"><a
+                                href="https://Pearsonlimoservices.ca/">www.Pearsonlimoservices.ca</a>
+                        </span>
+                    </p>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-10 col-10 py-3 OnlineBookig">
+                    <h3 class="Reaervationonline text-center">ONLINE RESERVATION</h3>
+                    <p class="AreaDetails mb-5">
+                        It is a long established fact that a reader will be distracted by
+                        the readable content of a page when looking at its layout.
+                    </p>
+						<div class="ReservationBox">
+							<p class="makeReservation m-auto"><a href="tel:+18889211116"><i class="fa fa-phone"></i>+1 (888) 921-1116</a></p>
+                    		<p class="makeReservation m-auto"><a href="tel:+14373707407"><i class="fa fa-mobile-phone"></i>+1 (437) 370-7407</a></p>
+						</div>
+						<div class="ReservationBox">
+							<p class="makeReservation m-auto"><a href="https://www.platinumrides.ca/reservation/"><i class="fa fa-car"></i>Make Reservation</a></p>
+                   			<p class="makeReservation m-auto"><a href="https://www.platinumrides.ca/fleet"><i class="fa fa-car"></i>Our Fleet</a></p>
+						</div>
+                    <div class="social_Links_Div ml-3">
+                        <a  href="https://www.facebook.com/platinumrides7"><i class="fa fa-facebook"></i></a>
+                        <a  href="https://twitter.com/platinumrides7"><i class="fa fa-twitter"></i></a>
+                        <a  href="http://platinumridesca@gmail.com"><i class="fa fa-google-plus"></i></a>
+                        <a  href="https://www.linkedin.com/in/shehroz-qamar7/"><i class="fa fa-linkedin"></i></a>
+                        <a  href="https://www.instagram.com/platinumrides7/?hl=en"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        </ul>
+                        <br /><br />
+                    </div>
+				</div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+					<hr>
+					<p>Ajax Airport Limo Service offers premium limousine experience, in an Executive Mercedes, Chrysler 300, Cadillac Escalade ESV,
+						 Rolls Royce Phantom, Chevrolet Suburban, Hummer Stretch Limousine, Shuttle Van, Minibus, and many more.
+						Our limousine fleet is astonishingly luxurious and our professionally trained chauffeurs are there to help you throughout your journey.
+						Regardless of the occasion, count on us to get you where you need to go. Getting a Limo in Ajax has never been easier; visit our website 
+						<a href="https://Pearsonlimoservices.ca/fleet">.Pearsonlimoservices.ca</a> and fill up your reservation.
+					</p>
+                    <p class="mb-2 AreaDetails">Our belief is in providing the highest quality limo experience to fulfill your requirements. Our team
+                        ensures a state-of-the-art limousine experience is delivered every single time you ride with the
+                        Ajax
+                        Limo service. We can provide a limousine at your doorstep in the Ajax for airport transfers,
+                        weddings,
+                        proms, parties, out of town travels, corporate events, and much more.
+                    </p>
+                    <hr>
+                    <h4 class="mb-2">“Mark your destinations and we will mark the difference”</h4>
+                    <p class="mb-2 AreaDetails">
+                        All our chauffeurs are licensed and insured professionals who work hard to ensure the safety and
+                        satisfaction of every client on each trip. The best customer service rating in the limousine
+                        industry of
+                        GTA, we arrive on time every time. If you are looking for the best limousine service in Ajax book
+                        your
+                        limo ride with Platinum Rides.
+                    </p>
+                    <hr>
+                    <h4 class="mb-2">Book your Limo Ride with Platinum Rides for ;</h4>
+                    <ul>
+                        <li>Toronto Airport Limo</li>
+                        <li>Travel to and from Toronto Airport (24/7/365)</li>
+                        <li>Executive Limo Transportation</li>
+                        <li>Theater and concert events</li>
+                        <li>Wedding Limo Rentals in Ajax</li>
+                        <li>Stags and Bachelorette party transportation</li>
+                        <li>Prom and Graduation Limo Rental</li>
+                        <li>Concerts Limo Rentals</li>
+                        <li>Sporting events such as Maple&nbsp;Leaf’s games, or&nbsp;Blue Jays Games</li>
+                        <li>Girls night out limo rental</li>
+                        <li>Guys night out limo rental</li>
+                        <li>Conferences, conventions and business meetings</li>
+                        <li>Casino Niagara trips</li>
+                        <li>Falls view Casino trips</li>
+                        <li>Casino Rama trips</li>
+                        <li>Ajax Sightseeing tours</li>
+                        <li>and so much more</li>
+                    </ul>
+                    <hr>
+                    <h4 class="mb-2 AreaDetails">If you need a limo… Call us now and reserve your limo!</h4>
+                    <p class=" mb-2 AreaDetails">Call Us! Our Ajax Limousine Service would be pleased to offer our airport limo service to pick you up
+                        and
+                        shuttle you to/from Toronto Pearson International Airport, Toronto City Centre Airport, Oshawa
+                        Airport,
+                        Buttonville Airport, or Downsview Airport, Hamilton Airport.
+					</p>
+					<hr>
+                    <h4 class="mb-2">Limo Rentals Around Ajax</h4>
+                    <p class="mb-2 AreaDetails">Are you looking for&nbsp;reliable airport transportation? Try Ajax Airport Limo Service.
+                        Our&nbsp;Airport
+                        Limo/Shuttle/ service&nbsp;provides Limousine Service to/from Ajax and the neighboring communities
+                        of
+                    </p>
+                    <hr>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12 text-center">
+                    Pickering, Ontario <br>
+                    Whitby, Ontario <br>
+                    Oshawa, Ontario <br>
+                    Markham, Ontario<br>
+                    Scarborough, Ontario <br>
+                    <hr>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12 text-center">
+                    Stouffville, Ontario<br>
+                    Bowmanville, Ontario<br>
+                    Port Perry, Ontario<br>
+                    Uxbridge, Ontario<br>
+                    Richmond Hill, Ontario<br>
+                    <hr>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12 text-center">
+                    Toronto, Ontario<br>
+                    Aurora, Ontario<br>
+                    Vaughan, Ontario<br>
+                    Newmarket, Ontario<br>
+                    Etobicoke, Ontario<br>
+                    <hr>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+<?php
+	include ('./footer.php');
+?>
---
+++ areaweserve/ajax.php
@@ -0,0 +1,9 @@
+<?php
+include ('./header.php');
+?>
+	<div class="container-fluid">
+		<h1>Hello World</h1>
+	</div>
+<?php
+	include ('./footer.php');
+?>
