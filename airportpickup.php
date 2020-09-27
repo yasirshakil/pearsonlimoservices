ab3773291151d2eb2ff0f1a9abfcb673ca866d85
shoaib8236 <shoaib.raza.dev@gmail.com>
2020-09-27 13:14:24 -0700

    Airport Pickup Page Added

---
+++ airportpickup.php
@@ -0,0 +1,98 @@
+<?php
+include ('./header.php');
+?>
+        <div class="container my-5 bgColor">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <h1 class="my-2 AjaxLimo">Airport Pickup Procedure</h1>
+                    <p class="mb-2 AreaDetails">
+                        Arriving at <a class="Highlight" href="">Toronto Pearson Airport</a>  from an International destination or from within Canada, let
+                        us take care of your ground transportation just as you arrive in <a class="Highlight" href="">Toronto</a> . You can pre-book your
+                        ride with Platinum Rides Airport Limo and travel to your destination hassle-free.
+                    </p>
+                    <p class="">
+                        Just as you plan to visit <a class="Highlight" href="">Toronto</a>  and you are in need of Ground Transportation from <a class="Highlight" href="">Toronto Pearson Airport</a> 
+                        Pearson Airport to any destination in Ontario, let us take care of<b> CALL us at</b> <a class="Highlight" href="tel:+1 (437) 370-7407">+1 (437) 370-7407</a>
+                        to pre-book your Toronto Airport Limo. You have the option of booking your Limo online by
+                        filling our <a class="Highlight" href="">online reservation form</a> or writing us an email <a class="Highlight" href="">reservation@platinumrides.ca</a> with
+                        your travel plan details. Our representative will assist you through simple procedures of
+                        booking a ride and book your Limo or luxury sedan, SUV, Stretch Limo, Mini Bus, Highway coach
+                        service from <a class="Highlight" href="">Toronto Pearson Airport</a> to your destination.
+                    </p>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12 py-3 OnlineBookig">
+                    <h3 class="Reaervationonline text-center">ONLINE RESERVATION</h3>
+                    <p class="AreaDetails mb-5">
+                        It is a long established fact that a reader will be distracted by
+                        the readable content of a page when looking at its layout.
+                    </p>
+                    <div class="ReservationBox">
+                        <p class="makeReservation m-auto"><a href="tel:+18889211116"><i class="fa fa-phone"></i>+1 (888) 921-1116</a></p>
+                        <p class="makeReservation m-auto"><a href="tel:+14373707407"><i class="fa fa-mobile-phone"></i>+1 (437) 370-7407</a></p>
+                    </div>
+                    <div class="ReservationBox">
+                        <p class="makeReservation m-auto"><a href="https://www.platinumrides.ca/reservation/"><i class="fa fa-car"></i>Make Reservation</a></p>
+                        <p class="makeReservation m-auto"><a href="https://www.platinumrides.ca/fleet"><i class="fa fa-car"></i>Our Fleet</a></p>
+                    </div>
+                    <div class="social_Links_Div ml-3">
+                        <a href="https://www.facebook.com/platinumrides7"><i class="fa fa-facebook"></i></a>
+                        <a href="https://twitter.com/platinumrides7"><i class="fa fa-twitter"></i></a>
+                        <a href="http://platinumridesca@gmail.com"><i class="fa fa-google-plus"></i></a>
+                        <a href="https://www.linkedin.com/in/shehroz-qamar7/"><i class="fa fa-linkedin"></i></a>
+                        <a href="https://www.instagram.com/platinumrides7/?hl=en"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                    <hr>
+                    <p class="mb-2">
+                        When You <b>Arrive at</b> <a class="Highlight" href="">Toronto Pearson Airport</a> you just need to follow a simple pickup procedure to
+                        get to your prearranged Limo service booked with Platinum Rides.
+                    </p>
+                    <h2 class="my-2">PICK UP Procedure: Toronto Pearson Airport (YYZ) Terminal 1.(Door A)</h2>
+                    <p class="mb-2 AreaDetails">Clear Customs, pick up your checked-in bags if any, and walk through the
+                        arrival hall, just as you step out of the arrival hall and come out to the terminal’s public
+                        area you will see the doors with alphabets. You need to look for <b>Door A</b>. you will see a
+                        <b>prearranged Limousine Counter at the Door A</b> inside Arrival hall Walk to the counter and inform
+                        the commissioner (officer) that you are pre-booked with Platinum Rides. The officer will then
+                        page your driver informing him of your arrival at the door. Your Limo will be released from the
+                        compound and your Limo will arrive at Door A in about 4 minutes. If you have trouble finding the
+                        prearranged Limousine counter or you need any assistance upon arrival, please don’t hesitate to
+                        CALL us at <a class="Highlight" href="tel:+1 (437) 370-7407">+1 (437) 370-7407</a> and we will assist you through the pickup procedure.
+                    </p>
+                    <hr>
+                    <h2>PICK UP Procedure: Toronto Pearson Airport (YYZ) Terminal 3.(Post 7)</h2>
+                    <p class="my-2 AreaDetails">Clear Customs, pick up your checked-in bags if any, and walk through the
+                        arrival hall, just as you step out of the arrival hall and come out to the terminal’s public
+                        area you will see the doors with alphabets. You need to <b>look for Door A</b>. Now you
+                        need to head to <b>POST 7</b>
+                    </p>
+                    <p class="mb-2 AreaDetails">
+                        When you reached to <b>Door A (POST 7)</b>  you will see a prearranged Limousine Counter at the Door A
+                        inside Arrival hall Walk to the counter and inform the commissioner (officer) that you are
+                        pre-booked with Platinum Rides. The officer will then page your driver informing him of your arrival
+                        at the door. Your Limo will be released from the compound and <b>your Limo will arrive at Door A (POST7) </b>
+                        in about 4 minutes. If you have trouble finding the prearranged Limousine counter or you need any
+                        assistance upon arrival, please don’t hesitate to CALL us at <a class="Highlight" href="tel:+1 (437) 370-7407">+1 (437) 370-7407</a> and we will
+                        assist
+                        you through the pickup procedure.
+                    </p>
+                    <hr>
+                    <h2 class="my-2">Meet and Greet Service: Toronto Pearson Airport (YYZ)</h2>
+                    <p class="mb-2 AreaDetails">
+                        We offer <b>Meet and Greet Service at Toronto Pearson Airport.</b> Platinum rides along with its
+                        airport limousine transfers provide a prestigious service of meet and greet at the airport. Are
+                        you expecting a VIP guest or you are new to Canada; we are there to give you a warm welcome. Our
+                        Courteous and Friendly Staff would be there at the arrival terminal to get your visit to Canada
+                        started. They will make you feel like home, take care of your luggage, assist with any queries
+                        and purchases, and escort to your luxurious limousine waiting for you at the exit. <b>Try it- It’s</b>
+                        great!
+                    </p>
+                </div>
+            </div>
+        </div>
+
+<?php
+	include ('./footer.php');
+?>
