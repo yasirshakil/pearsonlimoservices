8dbef81aa27cd396636192d4af33e321943173d7
shoaib8236 <shoaib.raza.dev@gmail.com>
2020-10-11 11:28:12 -0700

    Cadillac Esclade-detail

---
+++ Cadillac-Esclade.php
@@ -0,0 +1,120 @@
+<?php
+include ('./header.php');
+?>
+
+
+<body>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12 text-center mb-5">
+                <section class="template-title center">
+                    <h1 class="title has-over">Cadillac Escalade</h1>
+                    <span>Cadillac Escalade</span>
+                    <div class="co-lg-12 col-md-12 col-sm-12 col-12 mb-1 mt-1">
+                        <div class="Borders m-auto co-lg-4 col-md-4 col-sm-6 col-6"></div>
+                    </div>
+                </section>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12 col-12 Cars-Detail-imgBox">
+                <img src="images/cars/CADILLAC-ESCALADE2.jpg" alt="">
+            </div>
+            <div class="col-lg-1 col-md-1 col-sm-0 col-0">
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 col-12 Rates">
+                <div class="Rates-main-box">
+                    <span class="rates-heading">
+                        <img src="https://yyzlimo.ca/wp-content/plugins/gdlr-car/images/per-hour-rate.png" alt="">
+                        <h5 class=""> PER HOUR RATE</h5>
+                    </span>
+                    <span class="rates-heading2">
+                        <h6><b>$55 </b></h6>
+                        <p>/ HOUR <sub>*Minimum 2 Hr Service</sub></p>
+                        <p class=''> </p>
+                    </span>
+                    <hr>
+                </div>
+                <div class="Rates-main-box">
+                    <span class="rates-heading">
+                        <img src="https://yyzlimo.ca/wp-content/plugins/gdlr-car/images/per-day-rate.png" alt="">
+                        <h5 class=""> PER DAY RATE</h5>
+                    </span>
+                    <span class="rates-heading2">
+                        <h6><b>$700 </b></h6>
+                        <p>/ DAY <sub>*12 Hour Service</sub></p>
+                        <p class=''> </p>
+                    </span>
+                    <hr>
+                </div>
+                <div class="Rates-main-box">
+                    <span class="rates-heading">
+                        <img src="https://yyzlimo.ca/wp-content/plugins/gdlr-car/images/airport-transfer.png" alt="">
+                        <h5 class="">AIRPORT TRANSFER</h5>
+                    </span>
+                    <span class="rates-heading2">
+                        <h6><b>$55</b></h6>
+                        <p><sub>*Starting From</sub></p>
+                        <p class=''> </p>
+                    </span>
+                    <hr>
+                </div>
+            </div>
+            <div class="col-lg-7 col-md-12 col-sm-12 col-12 py-4 pr-5">
+                <h3 class="mb-2 limoColor">Cadillac Escalade:</h3>
+                <p class="mb-2">
+                Are you thinking of renting a Cadillac Escalade Limousine? If so, Platinum Rides has the largest selection of Cadillac Escalades in Greater Toronto. One of the most popular limos, the Cadillac Escalade is a real filibuster. When you see this ride arriving, you know that someone special is inside. Escalade limos can seat anywhere from 5-6 people depending on the stretch. It has more room than a normal limousine and it also allows your guests to have a much more comfortable:<br>
+                Whether you are looking to party with the boys or girls for that one last time or want to arrive in style to that prom? Platinum Rides Cadillac Escalade will help you make a grand entrance to that special event.
+                </p>
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 col-12 Cars-Detail p-5 my-4">
+                <h5 class="ml-2">Rolls Royce Phantom Details</h5>
+                <p>
+                <div class="About-Car">
+                    <i class="fa fa-male mr-3" aria-hidden="true"></i>Max. Passenger : 6<br>
+                </div>
+                <div class="About-Car">
+                    <i class=" fa fa-suitcase mr-2" aria-hidden="true "></i>Max. Baggage: 6<br>
+                </div>
+                <div class="About-Car">
+                    <i class=" fa fa-play mr-2" aria-hidden="true "></i>Flat screen TV’s, DVD player with CD player with AM<br>
+                    / FM surround sound, Plush leather seats
+                </div>
+                <div class="About-Car mb-4">
+                    <i class=" fa fa-car mr-1" aria-hidden="true "></i> Tinted Windows
+                </div>
+                </p>
+                <a class="Car-Detail-Btn ml-2 m-3" href="Reservation.php"> Make Reservation</a>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+                <p>
+                    Pearson Limo fleets also are daily inspected and maintained for your safety and comfort
+                    in mind. Please call any time to set up an appointment or place your airport transportation request
+                    online on the reservation page.
+                </p>
+                <hr>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                
+                <h3 class="mb-2">SPECIAL OFFERS</h3>
+                <ul>
+                    <li>We offer free stop of 10 minutes on the way to or from Airport.</li>
+                    <li>There will be no waiting charges for airport pickups.</li>
+                    <li>Wake Up Calls</li>
+                    <li>Coffee & News Paper</li>
+                    <li>Meet and greet at an airport for only 49.00</li>
+                </ul>
+                <hr>
+                <p>
+                    Don’t waste your time in searching of useless sites, go on Book now, and reserve your desired
+                    car, or Call us at <a class="Highlight" href="tel:905-974-9873">905-974-9873</a> or Tollfree <a
+                        class="Highlight" href="tel:888-900-3001">888-900-3001</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+<div class="scroll-top"></div>
+</div>
+<?php
+    include ('./footer.php');
+?>
+<!-- End Footer -->
