f089004ca423c968ad60fd5a87a9aeecb2c0942e
unknown <shoaib.raza.dev@gmail.com>
2020-10-14 13:11:56 -0700

    Services-rates

---
+++ Service-rates-mobile.php
@@ -0,0 +1,323 @@
+<?php
+include ('./header.php');
+?>
+<!-- Start Top Title -->
+<!-- <div class="col-lg-3 col-md-3 col-sm-6 col-6"></div> -->
+
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-lg-3 col-md-3 col-sm-6 col-6 image-section p-0">
+      <div class="tital-sapce"></div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      <div class="img-box">
+        <div class="img">
+          <img src="images/cars/24.jpeg" alt="" />
+        </div>
+        <div class="Cars-names">20 Passengers Limo Bus</div>
+      </div>
+      
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+      <div class="services-rates-tital"></div>
+      <div class="tital">
+        <h3 class="service-rates-heading">
+          PER HOUR RATE
+          <br />
+          <span>
+            <p>*Min 2 Hr Service + 13% HST</p>
+          </span>
+        </h3>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+      <div class="tital">
+        <h3 class="service-rates-heading">
+          PER DAY RATE
+          <br />
+          <span>
+            <p>eg. Wedding, Long trip</p>
+          </span>
+        </h3>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+      <div class="tital">
+        <h3 class="service-rates-heading">
+          AIRPORT TRANSFER
+          <br />
+          <span>
+            <p>*Flat Rate</p>
+          </span>
+        </h3>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+      <div class="Services-rates">
+        <div class><span class="price">$65</span> / hour</div>
+        <p class="Our-Services-Text">*Min 2 Hr Service + 13% HST</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+	include ('./footer.php');
+?>
+<!-- End Footer -->
