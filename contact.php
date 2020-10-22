40d3795dc502211a0c44b167fdad36ea91c991e7
yasir <yasir.shakeel@cooperativecomputing.com>
2020-10-22 19:39:36 +0500

    google map API location

--- contact.php
+++ contact.php
@@ -155,7 +155,7 @@ $publickey = "6LfYC88ZAAAAALDFxQp0cQUNfw3fljwfQOzx8Fee";
 		<hr>
 	</section>
 	<div class="w-100">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5771.065461733466!2d-79.813125!3d43.678687!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b1489f9bd91c3%3A0xac6fe621a7d305c1!2s17%20Willick%20Pl%2C%20Brampton%2C%20ON%20L6X%204Z5%2C%20Canada!5e0!3m2!1sen!2sus!4v1600577310613!5m2!1sen!2sus" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.3999231038333!2d-79.36181268450201!3d43.660651779121025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb4191ed16af%3A0x11604d7882791f23!2s20%20Tubman%20Ave%2C%20Toronto%2C%20ON%20M5A%200M8%2C%20Canada!5e0!3m2!1sen!2s!4v1603377300975!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 	</div>
 
 
