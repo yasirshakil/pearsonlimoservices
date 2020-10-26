794627b10aa7db1fa4dc47bf3d5092dfc3855285
shoaib <shoaib.raza.dev@gmail.com>
2020-10-26 06:32:01 -0700

    welcomme page or our fleet content changed

--- welcome.php
+++ welcome.php
@@ -9,7 +9,7 @@
 			<h1 class='limoColor mb-2'>00000</h1>
 			<p class="mb-5">Your conformation number for this trip</p>
 			<p class="reservationComplete mb-5">Your reservation request has been received and will be reviewed by our staff shortly. Should you have any questions or concerns, <br> Please feel free to contact us by phone at <a class="Highlight" href="tel:437-800-1547"> 437-800-1547</a></p>
-			<a class="bookTrip" href="">Book Another Trip</a> <br> <br>
+			<a class="bookTrip" href="Reservation.php">Book Another Trip</a> <br> <br>
 			<span><a class="Highlight mb-5" href="">Dont have an account? </a> <a class="Highlight" href="">create a new account</a></span><br>
 			<a class="Highlight" href="">Privacy Policy</a>
 		</div>
