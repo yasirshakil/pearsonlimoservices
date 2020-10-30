1c64a3d8862d3ed83c0b77865def879ea5bd3530
yasir <m.yasirshakil@gmail.com>
2020-10-31 02:30:55 +0500

    terms and condition

---
+++ termsandcondition.php
@@ -0,0 +1,39 @@
+<?php
+include ('./header.php');
+?>
+
+<section>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center"> Terms & Conditions </h1>
+            <h3> Reservation Agreement: </h3>
+            <p> A minimum 50% deposit is required upon the confirmation of your reservation. The company is not liable in the event of a mechanical breakdown while on the charter. Any lost time due to mechanical breakdown will be made up at a mutually agreed date. The client will assume full financial liability for any damage to the limousine during the duration of the rental by them or any member of their party. The driver reserves the right to terminate the trip without refund. It is illegal to stand within a moving vehicle. Alcohol consumption and drug use is prohibited by law in any vehicle in Ontario. Smoking is also not permitted in the vehicles. Any fines incurred will be paid for by the passengers. The company is not responsible for any articles left in the limousine. </p>
+            <h3> Additional Charges: </h3>
+            <p> For hourly reservations, any extra time used other than reserved for will be charge in 15-minute increments based on the hourly rate. Transfer reservations, which are based on distance rather than time, additional stops and waiting time will be at an extra charge based on the vehicle type.</p>
+            <h3> Airport Arrival Time: </h3>
+            <p>We allow a 45-minute wait time for domestic flights and a 1 hour wait time for international flights upon flight arrival time. Any additional time will be charged in
+                15-minute increments based on the hourly rate of the vehicle type.</p>
+            <h3>Cancellation:</h3>
+            <p>Cancellations must be received 72 hours prior to the scheduled pick-up time with valid reason. If the cancellation policy is not followed, the total amount will be charged for the reservation. For wedding and graduation reservations, the deposits will be non-refundable. The balance for these reservations will be taken up to a week before the date of the reservation and will then be non-refundable. A partial credit may apply for a future reservation, depending on circumstance regarding the date we receive the cancellation notice. Specialty vehicles, such as our Party Buses, require a minimum two-week cancellation notice for the deposit to be refundable. During the peak time (May to Sep) month of December and New Years Eve, we do require a minimum of two weeks notice to cancel a reservation for the deposit to be refundable.</p>
+            <h3>Vehicle Change:</h3>
+            <p>Depending on weather conditions, Platinum Rides Limousine. is not responsible for delays or cancellations caused by the weather due to unsafe road conditions (i.e. snow, traffic, accidents, etc.). The company withholds the right to dispatch a different vehicle type other than requested in bad weather conditions. The replacement vehicle will accommodate the number of passengers stated during the creation of your reservation</p>
+            <h3>Changes to the Trip:</h3>
+            <p>Any changes to a reservation must be communicated through our office. Changes made with a chauffeur are not final. Changes must be made through our office by e-mail or phone. Requests for changes by email must be sent at least 24hours before the scheduled pick-up time. Reservation changes by phone may be accepted with a 2-hour notice. All changes will depend on availability and may not be able to be made.</p>
+            <h3>Pet Policy:</h3>
+            <p>The company must be notified if the client is travelling with a pet. The pet must also be kept in a kennel for the duration of the trip. If not properly notified, the driver may refuse service due to personal issues, such as allergies to the pet. Another chauffeur will be arranged to complete the transfer. The company is not liable in the event of delays during this process.</p>
+            <h3>Vehicle Damage:</h3>
+            <p>The party paying for the reservation is responsible for all damages and/or cleaning charges incurred by the renter and/or party of the renter, including but not limited to: Vomit/Sickness $200, Alcohol spillage $100, smoking violation $200, Damage to upholstery/seats $500 - $1000, Opening a vehicle door into another vehicle or stationary object $1500 -$2000. Any charges incurred will be charged immediately to the credit provided for the reservation unless alternative arrangement is made by the client.</p>
+            
+            <p>Don’t waste your time in searching of useless sites, go on Book now, and reserve your desired car, or Call us at 437-800-1547 or Tollfree 888-900-3001</p>
+        </div>
+    </div>
+</div>
+</div>
+
+</section>
+
+<?php
+include ('./footer.php');
+?>
